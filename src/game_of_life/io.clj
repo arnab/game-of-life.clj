6521ea265614f57edeaf23595c2387d55b04e7ee
Arnab Deka <arnab.deka@gmail.com>
2014-04-14 16:19:14 +0530

    simpler way to construct cells.

--- src/game_of_life/io.clj
+++ src/game_of_life/io.clj
@@ -1,14 +1,27 @@
 (ns game-of-life.io
   (require [clojure.string :as str :only [split-lines]]))
 
-(defn- str-to-cell [sym]
+(defn- sym-to-cell-state
+  [sym]
   (case sym
-    "X" {:alive true :age 0}
-    "-" {:alive false :age 0}
+    "X" true
+    "-" false
     throw (IllegalArgumentException. (str "unknown pattern: " sym))))
 
-(defn- line-to-cells [s]
+(defn- str-to-cell
+  [sym]
+  {:age 0, :alive (sym-to-cell-state sym)}
+  )
+
+(defn- line-to-cells
+  [s]
   (map str-to-cell (re-seq #"\S" s)))
 
-(defn read-cells-from [filename]
+(defn read-cells-from
+  [filename]
+  "Read cells from a text file. Run as: `(read-cells-from 'examples/lwss.txt')`"
   (map line-to-cells (str/split-lines (slurp filename))))
+
+(defn display-board
+  [cells]
+  )
