63ea41f9e8192a2ce513d98870ebfed20aff8006
Arnab Deka <arnab.deka@gmail.com>
2014-04-14 17:45:46 +0530

    do not run the program while compiling.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,4 +1,5 @@
 (ns game-of-life.core
   (require [game-of-life.io :as io]))
 
-(io/visualize (io/read-cells-from "examples/lwss.txt"))
+#_ (io/read-cells-from "examples/lwss.txt")
+#_ (io/visualize (io/read-cells-from "examples/lwss.txt"))
