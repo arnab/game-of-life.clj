e45834583f012f67bfa5574a930ebfa623088602
Arnab Deka <arnab.deka@gmail.com>
2014-04-14 17:30:05 +0530

    way to visualize the current state.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,5 +1,4 @@
 (ns game-of-life.core
   (require [game-of-life.io :as io]))
 
-;; (io/read-cells-from "examples/pulsar.txt")
-(io/read-cells-from "examples/lwss.txt")
+(io/visualize (io/read-cells-from "examples/lwss.txt"))
--- src/game_of_life/io.clj
+++ src/game_of_life/io.clj
@@ -1,16 +1,15 @@
 (ns game-of-life.io
   (require [clojure.string :as str :only [split-lines]]))
 
-(defn- sym-to-cell-state
-  [sym]
-  (case sym
-    "X" true
-    "-" false
-    throw (IllegalArgumentException. (str "unknown pattern: " sym))))
+(def sym-to-state
+  {"X" :alive, "-" :dead})
+
+(def state-to-sym
+  (clojure.set/map-invert sym-to-state))
 
 (defn- str-to-cell
   [sym]
-  {:age 0, :alive (sym-to-cell-state sym)}
+  {:age 0, :state (sym-to-state sym)}
   )
 
 (defn- line-to-cells
@@ -22,6 +21,23 @@
   "Read cells from a text file. Run as: `(read-cells-from 'examples/lwss.txt')`"
   (map line-to-cells (str/split-lines (slurp filename))))
 
-(defn display-board
+(defn- cell-to-sym
+  [cell]
+  (state-to-sym (cell :state)))
+
+(defn- visualize-cells
   [cells]
-  )
+  (map #(str % " ")
+       (map cell-to-sym cells)))
+
+(defn visualize
+  ([rows-of-cells]
+     (visualize rows-of-cells ""))
+  ([rows-of-cells out]
+     (if (empty? rows-of-cells)
+       out
+       (visualize (rest rows-of-cells)
+                  (str out
+                       (reduce str
+                               (visualize-cells (first rows-of-cells)))
+                       "\n")))))
